cd6e6ec9e6e531ea91bf1b2e9c2f91989c0c61ab
Ricardo Reiter <ricardoreiterr@gmail.com>
2022-05-11 10:36:50 -0300

    first commit

--- App.js
+++ App.js
@@ -1,20 +1,21 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { View, Text, StyleSheet } from "react-native";
+import Header from "./src/components/Header";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Header />
+      <StatusBar />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
+  /* container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  }, */
 });
