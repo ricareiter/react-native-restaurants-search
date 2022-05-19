f277aa3809bbf0e9a71435998faf405e7950c7a7
Ricardo Reiter <ricardoreiterr@gmail.com>
2022-05-19 17:08:36 -0300

    update

--- src/components/Restaurants.js
+++ src/components/Restaurants.js
@@ -1,9 +1,22 @@
-import { View, Text } from "react-native";
+import { View, Text, Stylesheet } from "react-native";
 
 export default function Restaurants() {
   return (
-    <View>
-      <Text>Top Restaurants</Text>
+    <View style={styles.container}>
+      <Text style={styles.header}>Top Restaurants</Text>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    marginHorizontal: 25,
+    marginVertical: 15,
+    flex: 1,
+  },
+  header: {
+    fontWeight: "bold",
+    fontSize: 20,
+    paddingBottom: 10,
+  },
+});
